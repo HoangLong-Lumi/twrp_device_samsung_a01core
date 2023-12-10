41c8d57216653284124b3eb6722747c1537e844c
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-12-10 19:11:36 +0700

    a01core: Drop dynamic partition
    Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

--- device.mk
+++ device.mk
@@ -16,11 +16,3 @@
 # limitations under the License.
 #
 
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
